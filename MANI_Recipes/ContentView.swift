a7e8e34eb5bb91ea28fd37825b44581074826460
Mani Rastegari <mani.rastegari@gmail.com>
2024-11-07 11:22:41 -0500

    Initial Commit

---
+++ MANI_Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MANI_Recipes
+//
+//  Created by Mani Rastegari on 2024-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MANI_Recipes/MANI_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MANI_RecipesApp.swift
+//  MANI_Recipes
+//
+//  Created by Mani Rastegari on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct MANI_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
